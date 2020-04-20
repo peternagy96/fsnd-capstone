6bbcbdb34e79dbf3d7dd71ac6948bf3627c21ec2
Peter Nagy <peternagy96@gmail.com>
2020-04-20 19:04:52 +0200

    update naming and requirements

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from api import app
+from app import app
 from .database import db
 
 migrate = Migrate(app, db)
